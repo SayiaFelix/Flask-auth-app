1322705026b52e333913c21051927e9dbaf4ca97
SayiaFelix <sayia.lucas@student.moringaschool.com>
2022-05-12 08:08:20 +0300

    initial commit

--- project/__init__.py
+++ project/__init__.py
@@ -3,8 +3,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from .models import User
 
-# init SQLAlchemy so we can use it later in our models
+# init SQLAlchemy 
 db = SQLAlchemy()
 
 def create_app():
@@ -18,12 +19,12 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    from .models import User
-
+    
     @login_manager.user_loader
     def load_user(user_id):
         # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
+        
     # blueprint for auth routes in our app
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
